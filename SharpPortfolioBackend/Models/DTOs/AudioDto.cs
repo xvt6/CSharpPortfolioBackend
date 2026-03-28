02fa64e1f5503fe562e09da38a6c39e04a66602e
Chillout 6 <xvt6work@gmail.com>
2026-03-28 20:34:15 +0100

    implement audio file DTOs

--- SharpPortfolioBackend/Models/DTOs/AudioDto.cs
+++ SharpPortfolioBackend/Models/DTOs/AudioDto.cs
@@ -1,3 +1,28 @@
-﻿namespace SharpPortfolioBackend.Models.DTOs;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 
-public record AudioDto();
+namespace SharpPortfolioBackend.Models.DTOs;
+
+public record AudioDto(
+    long Id,
+    string DisplayName,
+    string Description,
+    string FileIdentifier,
+    float? BPM,
+    string? MusicKey,
+    List<string> Vibes
+);
+
+public record CreateAudioDto(
+    string Description,
+    float? BPM,
+    string? MusicKey,
+    List<string> Vibes,
+    IFormFile File
+);
+
+public record UpdateAudioDto(
+    string Description,
+    float? BPM,
+    string? MusicKey
+);
