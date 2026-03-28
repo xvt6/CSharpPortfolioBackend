8109ce1cadbbe36077d9a769d50b01b326ae23d5
Chillout 6 <xvt6work@gmail.com>
2026-03-28 21:14:01 +0100

    implement Post Entity

--- SharpPortfolioBackend/Models/Entities/Post.cs
+++ SharpPortfolioBackend/Models/Entities/Post.cs
@@ -2,5 +2,10 @@
 
 public class Post
 {
-    
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public List<AudioFile> AudioFiles { get; set; } = new();
 }
