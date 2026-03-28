4507642ae1c9b321994ff5265fe5a2df9730df91
Chillout 6 <xvt6work@gmail.com>
2026-03-28 18:16:33 +0100

    create a new oracle db user with permissions to create and alter tables

---
+++ SharpPortfolioBackend/Utils/fix_permissions.sql
@@ -0,0 +1,11 @@
+﻿-- Grant additional necessary permissions for migrations
+GRANT CREATE TABLE TO library;
+GRANT CREATE SEQUENCE TO library;
+GRANT CREATE VIEW TO library;
+GRANT CREATE PROCEDURE TO library;
+GRANT CREATE TRIGGER TO library;
+GRANT UNLIMITED TABLESPACE TO library;
+
+-- Often required for DB tools to work correctly
+GRANT RESOURCE TO library;
+GRANT CONNECT TO library;
