084aa78c6f6632f70f49224a19763ee762cd094f
Chillout 6 <xvt6work@gmail.com>
2026-03-28 20:33:04 +0100

    implement audio file entity

--- SharpPortfolioBackend/Models/Entities/AudioFile.cs
+++ SharpPortfolioBackend/Models/Entities/AudioFile.cs
@@ -1,6 +1,15 @@
 ﻿namespace SharpPortfolioBackend.Models.Entities;
+using System.Collections.Generic;
 
 public class AudioFile
 {
-    
+    public long Id { get; set; }
+    public string DisplayName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string FileIdentifier { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public float? BPM { get; set; }
+    public string? MusicKey { get; set; }
+    public virtual ICollection<Vibe> Vibes { get; set; } = new List<Vibe>();
 }
